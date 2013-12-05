d5d09107b05478ddd19c7a0bf0cc97952cf8a825
sachin <sachin.sehgal87@gmail.com>
2013-12-05 10:49:11 +0530

    users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  before_save {self.email = email.downcase}
   attr_accessible :email, :name
-  has_many :microposts
+  validates :name, presence: true, :length => {maximum: 15}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, :format => {:with => VALID_EMAIL_REGEX}, :uniqueness => {:case_sensitive => false}
 end
